ed5c0eb438a1cc31b0fac1333a65aa5a2e0408af
Abu Qais <abuqaiselegant@gmail.com>
2026-05-14 22:46:07 +0530

    added index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("hi");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.send("Exchange is running");
+});
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hi")
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Exchange is running");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
